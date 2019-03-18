88b6b31c75563308891d36b3289624ca3145bc65
xd <937913283@qq.com>
2019-03-18 11:14:05 +0800

    first test

---
+++ test2/src/main/java/com/xudong/test2/test2/App.java
@@ -0,0 +1,13 @@
+package com.xudong.test2.test2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
